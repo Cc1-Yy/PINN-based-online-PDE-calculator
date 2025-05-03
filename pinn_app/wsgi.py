279c4e9afc1f7f5d1818bb35205e1e6a9db1766d
Cc1-Yy <2542884@dundee.ac.uk>
2025-05-04 05:29:31 +0800

    Fix some bugs.

---
+++ pinn_app/wsgi.py
@@ -0,0 +1,18 @@
+import glob
+import os
+import shutil
+from pinn_app import create_app, init_logger, redirect_std_streams
+
+logger = init_logger()
+redirect_std_streams(logger)
+BASE_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(BASE_DIR, "pinn_app", "data")
+for path in glob.glob(os.path.join(DATA_DIR, "*")):
+    if os.path.isdir(path):
+        try:
+            shutil.rmtree(path)
+        except OSError as e:
+            pass
+
+app = create_app()
+server = app.server
